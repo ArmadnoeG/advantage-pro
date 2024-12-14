1578973b448b7bb0daab6409824f648977ad71e3
ArmadnoeG <echeverriagarmando@gmail.com>
2024-12-14 17:55:41 -0300

    Enhance UnitsPage layout with suspense and improve GridUnits component's structure and styling for better user experience

--- components/units/grid-units.tsx
+++ components/units/grid-units.tsx
@@ -20,23 +20,36 @@ export async function GridUnits() {
 	console.log(Object.entries(filteredUnits))
 
 	return (
-		<div className='grid grid-cols-7 w-full gap-4 '>
-			{Object.entries(filteredUnits).map(([unitType, units]) => (
-				<div
-					key={unitType}
-					className='flex flex-col gap-2 items-center border-x-[1px] border-accent p-2'
-				>
-					<h2 className='text-2xl font-bold'>{unitType}</h2>
-					{units.map(unit => (
-						<div
-							key={unit.id}
-							className='flex flex-col items-center justify-center bg-green-400 p-2 w-20 h-20 '
-						>
-							<h2 className='text-2xl font-bold'>{unit.name}</h2>
-						</div>
-					))}
-				</div>
-			))}
-		</div>
+		<>
+			<h2
+				className='font-[family-name:(var(--font-geist-sans))] text-2xl text-foreground font-bold mb-10 border-b-[1px]
+      border-border py-2'
+			>
+				Unidades
+			</h2>
+
+			<div className='grid grid-cols-7 w-full gap-2 '>
+				{Object.entries(filteredUnits).map(([unitType, units]) => (
+					<div
+						key={unitType}
+						className='flex flex-col gap-2 items-center border-x-[1px] border-border p-2'
+					>
+						<h2 className='text-xl font-semibold text-muted-foreground pb-8'>
+							{unitType}
+						</h2>
+						{units.map(unit => (
+							<div
+								key={unit.id}
+								className='flex flex-col items-center justify-center bg-green-500  w-28 h-8 rounded-md cursor-pointer'
+							>
+								<h2 className='text-lg from-accent-foreground font-[family-name:var(--font-geist-sans)] pointer-events-none'>
+									{unit.name}
+								</h2>
+							</div>
+						))}
+					</div>
+				))}
+			</div>
+		</>
 	)
 }
