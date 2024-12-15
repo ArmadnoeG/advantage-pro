2ad12edf56faa4aa988e0b25e212fbf3d19d49e9
ArmadnoeG <echeverriagarmando@gmail.com>
2024-12-15 01:21:38 -0300

    Add shadow effect to BoxUnit component for improved visual depth and styling consistency

--- components/units/box-unit.tsx
+++ components/units/box-unit.tsx
@@ -5,7 +5,7 @@ export function BoxUnit({ unit }: { unit: DBunit }) {
 	return (
 		<div
 			key={unit.id}
-			className={`flex flex-col items-center justify-center ${colorStatus(unit.status)}  w-[80%] h-8 rounded-md cursor-pointer transition-[colors, transform] duration-300 hover:scale-105`}
+			className={`flex flex-col items-center justify-center ${colorStatus(unit.status)}  w-[80%] h-8 rounded-md cursor-pointer transition-[colors, transform] duration-300 hover:scale-105 shadow-sm shadow-black/80 dark:shadow-white/80`}
 		>
 			<h2 className='text-lg from-accent-foreground font-[family-name:var(--font-geist-sans)] pointer-events-none'>
 				{unit.name}
