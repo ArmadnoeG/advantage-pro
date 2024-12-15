62ddea10975d7edb9c19a8aad25b81599d977beb
ArmadnoeG <echeverriagarmando@gmail.com>
2024-12-14 22:45:23 -0300

    Enhance BoxUnit with smooth hover effects for better user interaction and visual feedback in the component

--- components/units/box-unit.tsx
+++ components/units/box-unit.tsx
@@ -5,7 +5,7 @@ export function BoxUnit({ unit }: { unit: DBunit }) {
 	return (
 		<div
 			key={unit.id}
-			className={`flex flex-col items-center justify-center ${colorStatus(unit.status)}  w-[80%] h-8 rounded-md cursor-pointer`}
+			className={`flex flex-col items-center justify-center ${colorStatus(unit.status)}  w-[80%] h-8 rounded-md cursor-pointer transition-[colors, transform] duration-300 hover:scale-105`}
 		>
 			<h2 className='text-lg from-accent-foreground font-[family-name:var(--font-geist-sans)] pointer-events-none'>
 				{unit.name}
