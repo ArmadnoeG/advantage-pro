13b3f177945b58eda414918615c25f119a239608
ArmadnoeG <echeverriagarmando@gmail.com>
2024-12-19 02:48:10 -0300

    Implement search functionality and data fetching in manage-units page, add button for adding units, and refactor component logic

--- app/dashboard/manage-units/page.tsx
+++ app/dashboard/manage-units/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
 	Table,
 	TableBody,
@@ -10,24 +12,47 @@ import {
 import { getUnits } from '@/lib/db/actions/read'
 import { Edit, Search } from 'lucide-react'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { useState, useEffect, useMemo } from 'react'
+import { DBunit } from '@/types/db-types'
 
-export default async function PageManageUnits() {
-	const { data } = await getUnits()
+export default function PageManageUnits() {
+	const [data, setData] = useState<DBunit[]>([])
+	const [searchTerm, setSearchTerm] = useState('')
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const { data } = await getUnits()
+			if (!data) return
+			return setData(data)
+		}
+		fetchData()
+	}, [])
+
+	const filteredData = useMemo(() => {
+		if (!searchTerm) return data
+		return data.filter(unit =>
+			unit.name.toLowerCase().includes(searchTerm.toLowerCase())
+		)
+	}, [searchTerm, data])
 
 	return (
-		<div className='flex flex-col mx-auto px-4 w-full'>
-			<h1 className='text-3xl font-[family-name:var(--font-geist-sans)] font-bold m-5'>
+		<div className='flex flex-col mx-auto px-4 max-w-[90%]'>
+			<h1 className='text-3xl font-[family-name:var(--font-geist-sans)] font-bold my-5'>
 				Gestionar unidades
 			</h1>
-			<div className='flex justify-end mr-5 mb-5 relative'>
+			<div className='flex justify-between items-center mb-5 relative'>
+				<Button>Agregar unidad</Button>
 				<Input
 					placeholder='Buscar unidades...'
 					className='w-[300px]'
+					value={searchTerm}
+					onChange={e => setSearchTerm(e.target.value)}
 				/>
 				<Search className='absolute right-4 top-1/2 -translate-y-1/2 text-muted-foreground size-4' />
 			</div>
 
-			<Table className='max-w-5xl mx-auto'>
+			<Table className='mx-auto'>
 				<TableCaption className='text-sm text-muted-foreground mt-4'>
 					Gestiona las unidades de tu cuerpo de bomberos
 				</TableCaption>
@@ -41,7 +66,7 @@ export default async function PageManageUnits() {
 					</TableRow>
 				</TableHeader>
 				<TableBody>
-					{data?.map(unit => (
+					{filteredData?.map(unit => (
 						<TableRow
 							key={unit.id}
 							className='hover:bg-muted/50 transition-colors'
